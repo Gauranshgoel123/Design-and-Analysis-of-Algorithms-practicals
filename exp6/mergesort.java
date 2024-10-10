d44ae67c70dd1fde8382df6aae07c2036def15e6
Gauransh Goel <121503835+Gauranshgoel123@users.noreply.github.com>
2024-10-10 17:23:02 +0530

    Create mergesort.java

---
+++ exp6/mergesort.java
@@ -0,0 +1,68 @@
+class MergeSort {
+    void merge(int arr[], int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        int[] leftArr = new int[n1];
+        int[] rightArr = new int[n2];
+
+        for (int i = 0; i < n1; ++i)
+            leftArr[i] = arr[left + i];
+        for (int i = 0; i < n2; ++i)
+            rightArr[i] = arr[mid + 1 + i];
+
+        int i = 0, j = 0;
+        int k = left;
+
+        while (i < n1 && j < n2) {
+            if (leftArr[i] <= rightArr[j]) {
+                arr[k] = leftArr[i];
+                i++;
+            } else {
+                arr[k] = rightArr[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            arr[k] = leftArr[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr[k] = rightArr[j];
+            j++;
+            k++;
+        }
+    }
+
+    void sort(int arr[], int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+            sort(arr, left, mid);
+            sort(arr, mid + 1, right);
+            merge(arr, left, mid, right);
+        }
+    }
+
+    static void printArray(int arr[]) {
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]) {
+        int arr[] = { 12, 11, 13, 5, 6, 7 };
+        MergeSort ms = new MergeSort();
+        System.out.println("Given Array");
+        printArray(arr);
+
+        ms.sort(arr, 0, arr.length - 1);
+
+        System.out.println("\nSorted array");
+        printArray(arr);
+    }
+}
