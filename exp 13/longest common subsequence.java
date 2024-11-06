784a6fe4bb6915a80acd540996ccce164646a8ad
Gauransh Goel <121503835+Gauranshgoel123@users.noreply.github.com>
2024-11-06 21:49:17 +0530

    Create longest common subsequence.java

---
+++ exp 13/longest common subsequence.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class LCS {
+
+    static int lcs(String s1, String s2) {
+        int m = s1.length(), n = s2.length();
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        
+        return dp[m][n];
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter first string: ");
+        String s1 = scanner.nextLine();
+        System.out.print("Enter second string: ");
+        String s2 = scanner.nextLine();
+        scanner.close();
+        
+        System.out.println("Length of Longest Common Subsequence: " + lcs(s1, s2));
+    }
+}
